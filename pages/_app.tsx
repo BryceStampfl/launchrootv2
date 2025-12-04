d15b5a3f10f848ad3cba30f43e3f285d1c53d4c3
brycestampfl <brycestampfl@gmail.com>
2025-12-04 11:48:19 -0800

    updated title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={theme}>
       <Head>
-<<<<<<< HEAD
         <title>LaunchRoot - Business Solutions</title>
-=======
-        <title>Mantine Template</title>
->>>>>>> origin/master
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
